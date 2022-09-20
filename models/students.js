7516525370ce3056fe07fdf9d1eccbcc6c6061a4
madhan7383 <110127685+madhan7383@users.noreply.github.com>
2022-09-20 11:43:46 +0530

    initial commit

---
+++ models/students.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 33,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  enrollnumber: {
+    type: Number,
+    min: 1,
+    max: 120
+  },
+  department: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 25
+  }
+});
+
+module.exports = mongoose.model('students', studentSchema);
+
+
+
